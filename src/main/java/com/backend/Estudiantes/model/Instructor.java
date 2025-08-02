cdd670bf4df80b00fd32ca02a6c07f6d127971c2
Cristian <cmbarrera2242@gmail.com>
2025-08-01 21:20:06 -0500

    feat: entidad con atributos base de instructor

---
+++ src/main/java/com/backend/Estudiantes/model/Instructor.java
@@ -0,0 +1,33 @@
+package com.backend.Estudiantes.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "instructores")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Instructor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "usuario_id" , nullable = false , unique = true)
+    public Usuario usuario;
+
+    @Column(nullable = false)
+    private String especialiad;
+
+
+    private LocalDate fechaContratacion;
+
+}
