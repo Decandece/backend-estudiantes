e17ec3e6d501c6e617f8be1993a46a74eacb3995
Cristian <cmbarrera2242@gmail.com>
2025-08-06 16:30:12 -0500

    test

---
+++ src/main/java/com/backend/Estudiantes/service/UsuarioDetailsService.java
@@ -0,0 +1,37 @@
+package com.backend.Estudiantes.service;
+
+
+import com.backend.Estudiantes.model.Usuario;
+import com.backend.Estudiantes.repository.UsuarioRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class UsuarioDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        Usuario usuario = usuarioRepository.findByEmail(email)
+                .orElseThrow(() -> {
+                    log.error("Usuario no encontrado con email: {}", email);
+                    return new UsernameNotFoundException("Usuario no encontrado");
+                });
+
+        log.info("Usuario encontrado: {} con rol: {}", email, usuario.getRol());
+
+        return User.withUsername(usuario.getEmail())
+                .password(usuario.getPassword())
+                .roles(usuario.getRol().name())
+                .build();
+    }
+}
