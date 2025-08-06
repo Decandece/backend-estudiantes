a2a687692d79e9e098b62bb47f6bf33451a92276
Cristian <cmbarrera2242@gmail.com>
2025-08-06 16:27:46 -0500

    test

--- src/main/java/com/backend/Estudiantes/model/Estudiante.java
+++ src/main/java/com/backend/Estudiantes/model/Estudiante.java
@@ -41,4 +41,6 @@ public class Estudiante {
         }
         return "Nombre no disponible";
     }
+
+
 }
--- src/main/java/com/backend/Estudiantes/model/Usuario.java
+++ src/main/java/com/backend/Estudiantes/model/Usuario.java
@@ -13,8 +13,6 @@ import java.util.List;
 
 @Entity
 @Table(name = "usuario")
-@Getter
-@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Usuario implements UserDetails {
@@ -48,6 +46,63 @@ public class Usuario implements UserDetails {
         this.email = email;
         this.password = password;
         this.rol = rol;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRol() {
+        return rol;
+    }
+
+    public void setRol(Role rol) {
+        this.rol = rol;
+    }
+
+    public Boolean getActivo() {
+        return activo;
+    }
+
+    public void setActivo(Boolean activo) {
+        this.activo = activo;
     }
 
     //Devuelve los permisos del rol que tenga el usuario
