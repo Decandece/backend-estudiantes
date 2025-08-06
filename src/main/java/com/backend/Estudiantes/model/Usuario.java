bb4a801fc5bfd81999d093e76d73580baaa2d71f
Cristian <cmbarrera2242@gmail.com>
2025-08-06 11:48:28 -0500

    feat: implementacion de UserDetails en usuario

--- src/main/java/com/backend/Estudiantes/model/Usuario.java
+++ src/main/java/com/backend/Estudiantes/model/Usuario.java
@@ -5,6 +5,11 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name = "usuario")
@@ -12,7 +17,7 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class Usuario {
+public class Usuario implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -43,6 +48,43 @@ public class Usuario {
         this.email = email;
         this.password = password;
         this.rol = rol;
+    }
+
+    //Devuelve los permisos del rol que tenga el usuario
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+
+    //Nos devuelve el email del usuario - username (unico) como el email
+    @Override
+    public String getUsername() {
+        return "";
+    }
+
+    //Cuenta no expirada
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    //Cuenta no bloqueada
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    //Credenciales no expiradas
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    //Cuenta habilitada
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
     }
 
 }
